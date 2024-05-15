b7259cf583ce13c69ac8a6efe89f94f22c0d59fc
AnjanaTharanga24 <anjana24myprof@gmail.com>
2024-05-15 09:01:51 +0000

    add footer for the frontend

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 export default function LandingPage() {
   return (
     <div>
       <Navbar/>
+
+      <div className='' style={{marginTop:"40px"}}>
+        <h1>This is employee management landing page</h1>
+      </div>
+
+      <Footer/>
     </div>
   )
 }
